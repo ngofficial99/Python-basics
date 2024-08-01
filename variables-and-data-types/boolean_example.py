92d643284c06de521fbac9d2c55b81adab37eae4
Nishant Gupta <ngofficial99@gmail.com>
2024-08-01 16:07:51 +0530

    Boolean Example done

---
+++ variables-and-data-types/boolean_example.py
@@ -0,0 +1,23 @@
+"""
+Boolean Example in Python
+
+In Python, the Boolean data type represents one of two values: True or False. Booleans are used to evaluate conditions and make decisions in your code. They are fundamental to control flow and logical operations.
+
+This script demonstrates various operations and methods related to Boolean values. We'll explore:
+1. Creating and using Boolean variables.
+2. Logical operations using Boolean values.
+3. Comparison operations that result in Boolean values.
+4. Boolean conversion from other data types.
+
+One thing to make sure that True and False are reserved keywords and will only work when you write it with a Capital T and Capital F
+
+Let's dive into some examples to understand how Boolean values work and how they can be utilized in different scenarios.
+"""
+is_present = True 
+is_Absent = False
+
+print("Is Nishant Present in the office: ", is_present)
+print("Was Nishant Absent yesterday: ", is_Absent)
+
+
+
