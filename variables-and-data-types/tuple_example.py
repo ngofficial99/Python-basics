d9b6329d1f09e2975c7d32068d54568b879b63a0
Nishant Gupta <ngofficial99@gmail.com>
2024-08-01 18:42:08 +0530

    added introduction to Tuple file

---
+++ variables-and-data-types/tuple_example.py
@@ -0,0 +1,14 @@
+"""
+Tuple Example in Python
+
+In Python, a tuple is an immutable sequence of items. Tuples are similar to lists, but unlike lists, tuples cannot be changed after they are created (i.e., they are immutable). Tuples are typically used to store collections of heterogeneous data, meaning they can contain elements of different data types.
+
+This script demonstrates various operations and methods related to tuples. We'll explore:
+1. Creating and accessing tuple elements.
+2. Understanding the immutability of tuples.
+3. Common tuple operations and methods.
+4. Iterating over tuples.
+5. Using tuples for multiple assignments.
+
+Let's dive into some examples to understand how tuples work and how they can be utilized in different scenarios.
+"""
