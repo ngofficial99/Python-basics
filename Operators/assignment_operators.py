fa0498e94f554ee8cd1286b45f309f0111c3d2a6
Nishant Gupta <ngofficial99@gmail.com>
2024-08-05 15:29:52 +0530

    assignment operator done

---
+++ Operators/assignment_operators.py
@@ -0,0 +1,26 @@
+# assignment_operators.py
+
+# a = 10: This line assigns the value 10 to the variable a.
+# print("Initial value:", a): This line prints the initial value of a, which is 10.
+a = 10
+print("Initial value:", a)  # Output: 10
+
+# a += 5: This line adds 5 to the current value of a and assigns the result back to a. It is equivalent to a = a + 5.
+# print("After a += 5:", a): This line prints the new value of a, which is 15.
+a += 5
+print("After a += 5:", a)  # Output: 15
+
+# a -= 3: This line subtracts 3 from the current value of a and assigns the result back to a. It is equivalent to a = a - 3.
+# print("After a -= 3:", a): This line prints the new value of a, which is 12.
+a -= 3
+print("After a -= 3:", a)  # Output: 12
+
+# *a = 2: This line multiplies the current value of a by 2 and assigns the result back to a. It is equivalent to a = a * 2.
+# print("After a *= 2:", a): This line prints the new value of a, which is 24.
+a *= 2
+print("After a *= 2:", a)  # Output: 24
+
+# a /= 4: This line divides the current value of a by 4 and assigns the result back to a. It is equivalent to a = a / 4.
+# print("After a /= 4:", a): This line prints the new value of a, which is 6.0.
+a /= 4
+print("After a /= 4:", a)  # Output: 6.0
