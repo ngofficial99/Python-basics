22e5e5c9328c7f737c2df2801c68cbaba02a7f1a
Nishant Gupta <ngofficial99@gmail.com>
2024-08-05 15:11:29 +0530

    added arithematic operator

---
+++ Operators/arithmetic_operators.py
@@ -0,0 +1,15 @@
+# ### 1. Arithmetic Operators
+
+# Arithmetic operators are used to perform mathematical operations such as addition, subtraction, multiplication, and division.
+
+
+a = 10
+b = 5
+
+print("Addition:", a + b)         # Output: 15
+print("Subtraction:", a - b)      # Output: 5
+print("Multiplication:", a * b)   # Output: 50
+print("Division:", a / b)         # Output: 2.0
+print("Modulus:", a % b)          # Output: 0
+print("Exponentiation:", a ** b)  # Output: 100000
+print("Floor Division:", a // b)  # Output: 2
