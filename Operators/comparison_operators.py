aa5a69044389d8b45304d4e5ec2860f0fb10594f
Nishant Gupta <ngofficial99@gmail.com>
2024-08-05 15:32:19 +0530

    comparison operator done

---
+++ Operators/comparison_operators.py
@@ -0,0 +1,22 @@
+# comparison_operators.py
+
+a = 10
+b = 5
+
+print("a == b:", a == b)  # Output: False
+# Checks if a is equal to b. Since 10 is not equal to 5, the result is False.
+
+print("a != b:", a != b)  # Output: True
+# Checks if a is not equal to b. Since 10 is not equal to 5, the result is True.
+
+print("a > b:", a > b)    # Output: True
+# Checks if a is greater than b. Since 10 is greater than 5, the result is True.
+
+print("a < b:", a < b)    # Output: False
+# Checks if a is less than b. Since 10 is not less than 5, the result is False.
+
+print("a >= b:", a >= b)  # Output: True
+# Checks if a is greater than or equal to b. Since 10 is greater than 5, the result is True.
+
+print("a <= b:", a <= b)  # Output: False
+# Checks if a is less than or equal to b. Since 10 is not less than or equal to 5, the result is False.
