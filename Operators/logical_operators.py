43c3b7d1aa4fffa46c5786f374fcdb9f4d298707
Nishant Gupta <ngofficial99@gmail.com>
2024-08-05 16:24:24 +0530

    added logical operator

---
+++ Operators/logical_operators.py
@@ -0,0 +1,20 @@
+# logical_operators.py
+
+a = True
+b = False
+
+print("a and b:", a and b)  # Output: False
+# Performs a logical AND operation between a and b.
+# True and False results in False because both conditions need to be True for the result to be True.
+
+print("a or b:", a or b)    # Output: True
+# Performs a logical OR operation between a and b.
+# True or False results in True because at least one condition is True.
+
+print("not a:", not a)      # Output: False
+# Performs a logical NOT operation on a.
+# not True results in False because it negates the boolean value of a.
+
+print("not b:", not b)      # Output: True
+# Performs a logical NOT operation on b.
+# not False results in True because it negates the boolean value of b.
