a8a500cb7973afb5699b6915db593229829dc6ff
Nishant Gupta <ngofficial99@gmail.com>
2024-08-07 23:47:36 +0530

    added lists directory and basic list examples with readme file ( readme needs updating )

---
+++ Lists/accessing_list.py
@@ -0,0 +1,9 @@
+# we can access individual elements of a single dimensional list 
+
+# A single dimensional list is a list where elements are listed one after the other 
+#   Lets create a list from the multiples of 5 to 20 each element will be having a unique id called the index
+
+
+multiple_of_five = [5,10,15,20]
+
+# In this list the first element whos value is 5 will have the index 0, second will have 1 and so on. 
---
+++ Lists/list_example.py
@@ -0,0 +1,17 @@
+# To create a list we have to use [] brackets
+
+#  we can also create an empty list 
+
+numbers = []
+
+# We can create a normal list like 
+
+nums = [1,2,3,4,5,6,7,8,9,0]
+
+#  We can have dupleicate numbers in a list 
+
+duplicate_numbers = [1,1,2,2,3,3,4,4,55,55]
+
+#  we can have a list of different types of data items
+
+diff_items = [1,"nishant",True,2.3,"Mary had a little lamp"]
